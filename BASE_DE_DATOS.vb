c5872f6acd9435dbc63bae8e4595f4db34173a33
Crimcat <69399291+Crimtom@users.noreply.github.com>
2025-06-27 21:27:35 -0600

    Main_Master

---
+++ BASE_DE_DATOS.vb
@@ -0,0 +1,44 @@
+﻿Module BASE_DATOS
+    Public Db As New OleDb.OleDbConnection 'Esta es la variable que representa la Base de Datos
+    Public T As New DataSet '   T es la variable que trae informacion
+    Public SQL As String '  Esta es la variable string que se usa para importar/exportar informacion de la Base de Datos
+
+    Friend Sub CONECTAR()
+        Dim Ruta As String
+        Ruta = "File Name=" & Application.StartupPath & "\route.udl" '   Conecta la Base de Datos con el Proyecto
+        Db.ConnectionString = Ruta
+        Db.Open()
+    End Sub
+
+    Friend Sub DESCONECTAR() '  Desconecta la Base de Datos para que no quede abierta
+        Db.Close()
+    End Sub
+
+    Friend Sub CARGAR_TABLA(ByRef Tabla_Temporal As DataSet, ByVal Sql As String) ' Importa parametros de una tabla de la Base de Datos
+        CONECTAR()
+        Dim Consulta As New OleDb.OleDbDataAdapter(Sql, Db)
+        Consulta.Fill(Tabla_Temporal)
+        DESCONECTAR()
+    End Sub
+
+    Friend Sub EJECUTAR(ByVal Sql As String) '  Metodo para añadir, actualizar o quitar información de la Base de Datos
+        CONECTAR()
+        Dim Comando As New OleDb.OleDbCommand(Sql, Db)
+        Comando.ExecuteNonQuery()
+        DESCONECTAR()
+    End Sub
+    '   Crea una llave primaria para una tabla
+    Function PK(ByVal TABLA As String, ByVal ID As String) As Integer
+        T.Tables.Clear()
+        SQL = "SELECT " & ID & " FROM " & TABLA
+        CARGAR_TABLA(T, SQL)
+        If T.Tables(0).Rows.Count > 0 Then
+            T.Tables.Clear()
+            SQL = "SELECT MAX(ID) FROM " & TABLA
+            CARGAR_TABLA(T, SQL)
+            PK = T.Tables(0).Rows(0).ItemArray(0) + 1
+        Else
+            PK = 1
+        End If
+    End Function
+End Module
---
+++ FRM_MAIN.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FRM_MAIN
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.MenuStrip = New System.Windows.Forms.MenuStrip()
+        Me.FileMenu = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStrip = New System.Windows.Forms.ToolStrip()
+        Me.ToolTip = New System.Windows.Forms.ToolTip(Me.components)
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.IniciarSesiónToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CrearUsuarioToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip
+        '
+        Me.MenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileMenu, Me.ExitToolStripMenuItem})
+        Me.MenuStrip.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip.Name = "MenuStrip"
+        Me.MenuStrip.Size = New System.Drawing.Size(1430, 24)
+        Me.MenuStrip.TabIndex = 5
+        Me.MenuStrip.Text = "MenuStrip"
+        '
+        'FileMenu
+        '
+        Me.FileMenu.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.IniciarSesiónToolStripMenuItem, Me.CrearUsuarioToolStripMenuItem})
+        Me.FileMenu.ImageTransparentColor = System.Drawing.SystemColors.ActiveBorder
+        Me.FileMenu.Name = "FileMenu"
+        Me.FileMenu.Size = New System.Drawing.Size(48, 20)
+        Me.FileMenu.Text = "&Inicio"
+        '
+        'ToolStrip
+        '
+        Me.ToolStrip.Location = New System.Drawing.Point(0, 24)
+        Me.ToolStrip.Name = "ToolStrip"
+        Me.ToolStrip.Size = New System.Drawing.Size(1430, 25)
+        Me.ToolStrip.TabIndex = 6
+        Me.ToolStrip.Text = "ToolStrip"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(41, 20)
+        Me.ExitToolStripMenuItem.Text = "&Salir"
+        '
+        'IniciarSesiónToolStripMenuItem
+        '
+        Me.IniciarSesiónToolStripMenuItem.Name = "IniciarSesiónToolStripMenuItem"
+        Me.IniciarSesiónToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.IniciarSesiónToolStripMenuItem.Text = "Iniciar Sesión"
+        '
+        'CrearUsuarioToolStripMenuItem
+        '
+        Me.CrearUsuarioToolStripMenuItem.Name = "CrearUsuarioToolStripMenuItem"
+        Me.CrearUsuarioToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.CrearUsuarioToolStripMenuItem.Text = "Crear Usuario"
+        '
+        'FRM_MAIN
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1430, 617)
+        Me.Controls.Add(Me.ToolStrip)
+        Me.Controls.Add(Me.MenuStrip)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.IsMdiContainer = True
+        Me.MainMenuStrip = Me.MenuStrip
+        Me.Name = "FRM_MAIN"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "FRM_MAIN"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.MenuStrip.ResumeLayout(False)
+        Me.MenuStrip.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ToolTip As System.Windows.Forms.ToolTip
+    Friend WithEvents ToolStrip As System.Windows.Forms.ToolStrip
+    Friend WithEvents FileMenu As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents MenuStrip As System.Windows.Forms.MenuStrip
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents IniciarSesiónToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CrearUsuarioToolStripMenuItem As ToolStripMenuItem
+End Class
